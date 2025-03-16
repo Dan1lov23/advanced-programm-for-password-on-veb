f9e96d5924f96b31bccbb2db318758edefea0821
kitik <svy1dan@gmail.com>
2025-03-16 21:11:04 +0300

    main javaScript file

---
+++ main/index.js
@@ -0,0 +1,78 @@
+function main() {
+
+    const letters = document.getElementById("letters").value;
+    const numbers = document.getElementById("numbers").value;
+    const symbols = document.getElementById("symbols").value;
+
+    let resultPassword = "";
+
+    let lettersCounter = 0;
+    let numbersCounter = 0;
+    let specialSymbolCounter = 0;
+
+    const numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+    const lettersArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z'
+    ];
+    const specialCharactersArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+        '-', '_', '=', '+', '{', '}', '[', ']', '|', ':', ';', '"', '&lt;', '&gt;', '?',
+        ',', '.', '/', '~', '`'];
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    if (Number(letters) > 0) {
+        while (lettersCounter <= Number(letters) - 1) {
+            resultPassword += lettersArray[getRandomInt(0, (lettersArray.length - 1))];
+            lettersCounter++;
+        }
+    }
+
+    if (Number(numbers) > 0) {
+        while (numbersCounter <= Number(numbers) - 1) {
+            resultPassword += numbersArray[getRandomInt(0,(numbersArray.length - 1))];
+            numbersCounter++;
+        }
+    }
+
+    if (Number(symbols) > 0) {
+        while (specialSymbolCounter <= Number(symbols) - 1) {
+            resultPassword += specialCharactersArray[getRandomInt(0, (specialCharactersArray.length - 1))];
+            specialSymbolCounter++;
+        }
+    }
+
+
+    document.getElementById("resultPassword").innerHTML = `Ваш пароль - ${resultPassword}`;
+
+    // модуль проверки
+
+    function checkPasswordLevel1(resultPassword) {
+        for (let a = 0; a < resultPassword.length; a++) {
+            if (resultPassword[a] === resultPassword[a + 1] && resultPassword[a] === resultPassword[a + 2]) {
+                console.log("3 повторяющихся символа подряд представляют угрозу безопасности");
+            }
+        }
+        for (let a = 0; a < resultPassword.length; a++) {
+            let checkSymbol = resultPassword[a];
+            let occurrencesCounter = 0;
+            for (let a = 0; a < resultPassword.length; a++) {
+                if (checkSymbol === resultPassword[a]) {
+                    occurrencesCounter++;
+                }
+                if (occurrencesCounter => 4) {
+                    console.log("4 одинаковых символа в пароле представляют угрозу безопасности");
+                }
+            }
+        }
+    }
+}
